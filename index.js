b2ac6ebfd1e73bcc06547c6541496c4d8900c85f
Pan2209 <mazuniga@sudamericano.edu.ec>
2024-05-14 09:28:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict';
+const claveCorrecta = 3214;
+let intentos = 3;
+
+
+while(intentos > 0){
+    let claveIngresada = prompt('INGRESE LA CLAVE');
+
+    if (claveIngresada == claveCorrecta) {
+        alert("Clave correcta, acceso concedido.");
+        break;
+    }else{
+        alert("La clave ingresada es incorrecta. Intente nuevamente.");
+        intentos --;
+    
+    }
+}
+
+if (intentos == 0) {
+    alert("Se ha quedado sin intentos. Su cuenta fue bloqueada.")
+}
